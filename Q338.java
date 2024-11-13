e5c332671eb919e42d0b18143a3ae86736c0cc02
Sajid Ali Khan <skprogramminghi@gmail.com>
2024-11-13 21:50:49 +0500

    new solution Q342 added

--- Q338.java
+++ Q338.java
@@ -1,15 +1,21 @@
 import java.util.*;
 public class Q338 {
-    public int[] countBits(int n) {
+    public static int[] countBits(int n) {
+        int[] ans = new int[n + 1];
 
-
-        int[] ans = new int[n+1];
-        for (int i=0; i<ans.length; i++){
-
-            
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
 
         return ans;
+    }
 
+    public static void main(String[] args) {
+
+        int[] arr = countBits(2);
+        for (int i: arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
---
+++ Q342.java
@@ -0,0 +1,9 @@
+public class Q342 {
+    public static boolean isPowerOfFour(int n) {
+        return n > 0 && (n & (n - 1)) == 0 && (n & 0x55555555) != 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPowerOfFour(16));
+    }
+}
