32c0b20d51b5b0cb98225b3e41aef31253247c85
Sajid Ali Khan <skprogramminghi@gmail.com>
2025-01-04 23:24:11 +0500

    new solution Q1930 added

---
+++ Q1930.java
@@ -0,0 +1,32 @@
+import java.util.HashSet;
+import java.util.Set;
+public class Q1930 {
+
+    public static int countPalindromicSubsequence(String s) {
+        int n = s.length();
+        int[] left = new int[26];
+        int[] right = new int[26];
+        for (char c : s.toCharArray()) {
+            right[c - 'a']++;
+        }
+
+        Set<String> uniquePalindromes = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            char middle = s.charAt(i);
+            right[middle - 'a']--;
+            for (int c = 0; c < 26; c++) {
+                if (left[c] > 0 && right[c] > 0) {
+                    uniquePalindromes.add((char) (c + 'a') + "" + middle + (char) (c + 'a'));
+                }
+            }
+            left[middle - 'a']++;
+        }
+
+        return uniquePalindromes.size();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countPalindromicSubsequence("aabca"));
+
+    }
+}
